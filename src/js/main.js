a95b205813873d9c6366455d093cdddc76ef1bbd
leonieschmutz <98399733+leonieschmutz@users.noreply.github.com>
2023-11-16 11:05:13 +0100

    mise en place structure avec scroll horizontal

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,17 @@
 import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+// import { Timeline } from "gsap/gsap-core";
+
+gsap.registerPlugin(ScrollTrigger);
 
 const n = 3;
 var amplitudeX = (-100 * (n - 1)) / n;
 
-gsap.to(".slider-track", {
+let tl = gsap.timeline({});
+
+const blockScroll = document.querySelector(".slider-mask-2");
+
+tl.to(".slider-track", {
   scrollTrigger: {
     trigger: ".slider-mask",
     start: "top top",
@@ -11,7 +19,46 @@ gsap.to(".slider-track", {
     pin: true,
     scrub: true,
     markers: true,
+    onComplete: () => {
+      // blockScroll.classList.add("slider-mask-2-hidden");
+    },
   },
   x: amplitudeX + "%",
   ease: "sine.inOut",
+});
+
+var slider1Button = document.querySelector("#slider-1-button");
+
+slider1Button.addEventListener("click", function () {
+  document.querySelector(".slider-mask-2").classList.remove("hidden");
+  tl.to(".slider-track-2", {
+    scrollTrigger: {
+      trigger: ".slider-mask-2",
+      start: "top top",
+      end: "+=300%",
+      pin: true,
+      scrub: true,
+      markers: true,
+    },
+    x: amplitudeX + "%",
+    ease: "sine.inOut",
+  });
+});
+
+var slider2Button = document.querySelector("#slider-2-button");
+
+slider2Button.addEventListener("click", function () {
+  document.querySelector(".slider-mask-3").classList.remove("hidden");
+  tl.to(".slider-track-3", {
+    scrollTrigger: {
+      trigger: ".slider-mask-3",
+      start: "top top",
+      end: "+=300%",
+      pin: true,
+      scrub: true,
+      markers: true,
+    },
+    x: amplitudeX + "%",
+    ease: "sine.inOut",
+  });
 });
